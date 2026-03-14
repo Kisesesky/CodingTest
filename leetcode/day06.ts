b5b56f39ca2a66a6383b46abb43ff6a2d11ad6bc
한성호 <hoya@Hoya-MacBook-Air.local>
2026-03-14 18:34:36 +0900

    fix: 폴더세분화

---
+++ leetcode/day06.ts
@@ -0,0 +1,52 @@
+// 20. Valid Parentheses
+// https://leetcode.com/problems/valid-parentheses/description/
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+// Example 1:
+// Input: s = "()"
+// Output: true
+
+// Example 2:
+// Input: s = "()[]{}"
+// Output: true
+
+// Example 3:
+// Input: s = "(]"
+// Output: false
+
+// Example 4:
+// Input: s = "([])"
+// Output: true
+
+// Example 5:
+// Input: s = "([)]"
+// Output: false
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'.
+
+function isValid(s: string): boolean {
+  let stack: string[] = [];
+
+  for(let char of s){
+    if(char === '(' || char === '{' || char === '[') {
+        stack.push(char)
+    } else {
+      let last = stack.pop();
+      if (!last) return false;
+      if((char === ')' && last !== '(') || (char === '}' && last !== '{') || (char === ']' && last !== '[')){
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
